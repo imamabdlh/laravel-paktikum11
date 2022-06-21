b1dda578b2760cd8ecd329e8706a62ef27aaf320
imamabdlh <imamabdillah2604@gmail.com>
2022-06-21 18:06:48 +0700

    laravel-praktikum11

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')  
+
+@section('content')  
+
+<div class="container">  
+        <h3>Daftar Mahasiswa</h3>  
+        
+        <table class="table table-bordered"> 
+        <a href="{{ url('mahasiswa/create') }}">Tambah Data</a>
+            <tr>  
+                <td>NIM</td>  
+                <td>NAMA</td>  
+                <td>JURUSAN</td>  
+                <td>ALAMAT</td>  
+                <td>EDIT</td>
+                <td>HAPUS</td>
+            </tr>  
+            @foreach($rows as $row)  
+            <tr>
+                  <td>{{ $row->mhsw_nim }}</td>  
+                  <td>{{ $row->mhsw_nama }}</td>  
+                  <td>{{ $row->mhsw_jurusan }}</td>  
+                  <td>{{ $row->mhsw_alamat }}</td>
+                  <td><a href="{{ url('mahasiswa/' . $row->mhsw_id . '/edit') }}">Edit</a></td>
+                  <td>
+                      <form action="{{ url('mahasiswa/' . $row->mhsw_id) }}" 
+                    method="POST">  
+                    @method('DELETE')  
+                    @csrf  
+                    <button>Hapus</button>  
+                </form> 
+            </td>
+                </tr>
+            @endforeach  
+        </table>  
+    </div>  
+@endsection
